a1d9c91ca3e0fc9ef1b0857bef6828c3f10f3b91
Ar2nite <a.van.alonso@gmail.com>
2022-02-09 21:44:20 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+double calculate(std::string filename) {
+
+    std:: ifstream { filename };
+    if (input) {//if open? can we read? is it not empty?
+        std::string name{}, catagory{};
+        char gender{};
+        int age{};
+        // while (input) {// as long as I can read from input
+    }
+    while(input<<input >> word >> age >> gender >> catagory) {
+        std::cout << name << " " << catagory << '\n'
+    }
